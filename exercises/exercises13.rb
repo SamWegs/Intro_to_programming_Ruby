80dbe4ecaa52bc3229e8fa1baebcb825e915ae5d
SamWegs <samwegleitner@gmail.com>
2020-01-20 12:42:10 -0700

    Completed the remainder of the exercises from book 1

---
+++ exercises/exercises13.rb
@@ -0,0 +1,8 @@
+# exercises13.rb
+
+a = ['white snow', 'winter wonderland', 'melting ice',
+     'slippery sidewalk', 'salted roads', 'white trees']
+
+b = a.map { |word| word.split }
+
+p b.flatten
